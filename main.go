f6eddccef7c7a7b9242eba2ca045b78e3baf5177
Douglas R. Ferreira <drfdouglas@outlook.com>
2022-01-31 12:08:40 -0300

    Brazil timezone

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"fmt"
 	"stockpredictionscronjob/service"
+	"time"
 
 	"github.com/robfig/cron/v3"
 )
 
 func main() {
 	fmt.Println("Automation data handler is running...")
-	c := cron.New(cron.WithSeconds())
+
+	sptz, _ := time.LoadLocation("America/Sao_Paulo")
+	c := cron.New(cron.WithLocation(sptz))
 
 	service.BootstrapFirstHistories()
 	fmt.Println("Bootstrap finished")
 
-	c.AddFunc("0 20 10 * * *", service.SaveLastStockPrices)
-	c.AddFunc("0 * 11-18 * * *", service.UpdateLastStockPrices)
+	c.AddFunc("15 10 * * MON-FRI", service.SaveLastStockPrices)
+	c.AddFunc("* 11-18 * * MON-FRI", service.UpdateLastStockPrices)
 
-	c.AddFunc("0 20 18 * * *", service.UpdatePrdictionLog)
-	c.AddFunc("0 30 18 * * *", service.MakePredictions)
+	c.AddFunc("15 18 * * MON-FRI", service.UpdatePrdictionLog)
+	c.AddFunc("30 18 * * MON-FRI", service.MakePredictions)
 
 	fmt.Println(c.Entries())
 
